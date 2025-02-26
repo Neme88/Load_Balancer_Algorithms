5841a0e0d765c0048b1d9a55976dced26eadf810
Neme Nwaka <ccnwaka1988@gmail.com>
2025-02-26 20:40:32 +0200

    Feat: add logic class that gets server by index

--- WeightedRoundRobin.py
+++ WeightedRoundRobin.py
@@ -4,3 +4,9 @@ class WeightedRoundRobinLoadBalancer:
         for server, weight in server_weights.items():
             self.servers.extend([server] * weight)  # Duplicate servers according to weight
         self.index = 0
+    
+    def get_server(self):
+        server = self.servers[self.index]
+        self.index = (self.index + 1) % len(self.servers)
+        return server
+    
