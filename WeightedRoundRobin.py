b5cfa95ff378ff9a0eaf763159fdd7b0399de3e3
Neme Nwaka <ccnwaka1988@gmail.com>
2025-02-26 20:42:32 +0200

    Test: Add test cases for weighted Round Robin algorithm

--- WeightedRoundRobin.py
+++ WeightedRoundRobin.py
@@ -9,4 +9,12 @@ class WeightedRoundRobinLoadBalancer:
         server = self.servers[self.index]
         self.index = (self.index + 1) % len(self.servers)
         return server
+
+
+
+# Example Usage
+server_weights = {"Server1": 3, "Server2": 1, "Server3": 2}
+lb = WeightedRoundRobinLoadBalancer(server_weights)
+for _ in range(6):
+    print(f"Request handled by: {lb.get_server()}")
     
