fd8e488ef466fe190e46ab1784064e3343a8463d
Neme Nwaka <ccnwaka1988@gmail.com>
2025-02-27 22:33:55 +0200

    Feat: add the core logic class method

--- IPHashingLoadBalancer.py
+++ IPHashingLoadBalancer.py
@@ -3,3 +3,7 @@ import hashlib
 class IPHashingLoadBalancer:
     def __init__(self, servers):
         self.servers = servers
+    def get_server(self, client_ip):
+        hash_value = int(hashlib.md5(client_ip.encode()).hexdigest(), 16)
+        index = hash_value % len(self.servers)
+        return self.servers[index] 
